60de1523a58c2f53d835cc09866a1d987e328b9c
theEpicMouse90 <68754253+theEpicMouse90@users.noreply.github.com>
2023-01-16 20:35:03 -0600

    Changed fileJ

--- FileJ.java
+++ FileJ.java
@@ -1,8 +1,8 @@
-package Java;
+package LearnJava;
 
 import java.io.File;
 
-public class File {
+public class FileJ {
     public static void main(String[] args){
         System.out.println("h");
     }
